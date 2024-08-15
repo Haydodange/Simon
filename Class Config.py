2b65d8de16b32bebec0c85794c8ac13b32472c4b
HaydoDanger <hayden.bell12@education.nsw.gov.au>
2024-08-15 13:00:43 +1000

    Minor class sturcture update
    Tried to create a Game parent class for most coding logic rather than call each individually. Minor changes to init for Software and Hardware

--- Class Config.py
+++ Class Config.py
@@ -24,14 +24,18 @@ bPin = #type:ignore
 piezoPin = #type:ignore
 
 
-class Hardware:
+class Gameplay:
+    pass
 
-    def __init__(self):
+class Hardware(Gameplay):
+
+    def __init__(self, softwareClass):
         self.gPin = gPin
         self.rPin = rPin
         self.yPin = yPin
         self.bPin = bPin
         self.piezoPin = piezoPin
+        self.softwareClass = softwareClass
     
     def ShowSequence(self):
         pass
@@ -46,7 +50,7 @@ class Hardware:
         pass
 
 
-class Software:
+class Software(Gameplay):
 
     def __init__(self, hardwareClass):
         self.sequence = sequence
