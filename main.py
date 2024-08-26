4878003b9988f214c41dbef1e75ddfc41884aaa1
Wealiums <Brodie.weal@education.nsw.gov.au>
2024-08-26 15:06:28 +1000

    More Run Throughs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 from ClassConfig import *
-
+from gpio_lcd import GpioLcd
 
 software = Software()
 hardware = Hardware()
@@ -11,8 +10,32 @@ level = 1
 note = 0
 speed = 1050
 
+lcd = GpioLcd(rs_pin = Pin(3),
+              enable_pin = Pin(2),
+              d0_pin = Pin(11),
+              d1_pin = Pin(10),
+              d2_pin = Pin(9),
+              d3_pin = Pin(8),
+              d4_pin = Pin(7),
+              d5_pin = Pin(6),
+              d6_pin = Pin(5),
+              d7_pin = Pin(4),
+              num_lines = 2, num_columns = 16)
+
+#while resetButton.value() == False:
+    #lcd.putstr("Press Start")
+    
+
 software.GenerateSequence()
 hardware.ShowSequence(sequence)
 hardware.GetSequence()
-
+software.GenerateSequence()
+hardware.ShowSequence(sequence)
+hardware.GetSequence()
+software.GenerateSequence()
+hardware.ShowSequence(sequence)
+hardware.GetSequence()
+software.GenerateSequence()
+hardware.ShowSequence(sequence)
+hardware.GetSequence()
 
