be8aa8e1fc6dc312787cc5238933ae694bad9db1
HaydoDanger <hayden.bell12@education.nsw.gov.au>
2024-08-26 10:38:51 +1000

    LCD additions
    Created the LCD libraries to make the LCD work (duh), created the LCD test py file and edited the original test file to support the LCD display.

---
+++ LCDHelloWorldTest.py
@@ -0,0 +1,20 @@
+from machine import Pin
+from gpio_lcd import GpioLcd
+import utime
+
+#Configure the Gpio pin of the Raspberry pi pico
+lcd = GpioLcd(rs_pin = Pin(3),
+              enable_pin = Pin(2),
+              d0_pin = Pin(11),
+              d1_pin = Pin(10),
+              d2_pin = Pin(9),
+              d3_pin = Pin(8),
+              d4_pin = Pin(7),
+              d5_pin = Pin(6),
+              d6_pin = Pin(5),
+              d7_pin = Pin(4),
+              num_lines = 2, num_columns = 16)
+#print the string on the display
+lcd.putstr('Simon Level 10000')
+
+
--- Test.py
+++ Test.py
@@ -1,5 +1,19 @@
 from machine import Pin, PWM
 from time import sleep
+from gpio_lcd import GpioLcd
+
+lcd = GpioLcd(rs_pin = Pin(3),
+              enable_pin = Pin(2),
+              d0_pin = Pin(11),
+              d1_pin = Pin(10),
+              d2_pin = Pin(9),
+              d3_pin = Pin(8),
+              d4_pin = Pin(7),
+              d5_pin = Pin(6),
+              d6_pin = Pin(5),
+              d7_pin = Pin(4),
+              num_lines = 2, num_columns = 16)
+
 
 gSound = 200
 rSound = 300
@@ -20,38 +34,44 @@ resetButton = Pin(16, Pin.IN)
 while True:
     if rButton.value() == True:
         print("Red")
+        lcd.putstr("Red")
         rPin.on()
-        piezoPin.duty_u16(500)
+        piezoPin.duty_u16(200)
         piezoPin.freq(200)
         sleep(1.2)
     elif yButton.value() == True:
         print("Yellow")
+        lcd.putstr("Yellow")
         rPin.on()
         gPin.on()
-        piezoPin.duty_u16(500)
+        piezoPin.duty_u16(200)
         piezoPin.freq(300)
         sleep(1.2)
     elif gButton.value() == True:
         print("Green")
+        lcd.putstr("Green")
         gPin.on()
-        piezoPin.duty_u16(500)
+        piezoPin.duty_u16(200)
         piezoPin.freq(400)
         sleep(1.2)
     elif bButton.value() == True:
         print("Blue")
+        lcd.putstr("Blue")
         bPin.on()
-        piezoPin.duty_u16(500)
+        piezoPin.duty_u16(200)
         piezoPin.freq(500)
         sleep(1.2)
     elif resetButton.value() == True:
         print("Start")
+        lcd.putstr("Start")
         gPin.on()
         bPin.on()
         rPin.on()
-        piezoPin.duty_u16(800)
+        piezoPin.duty_u16(200)
         piezoPin.freq(600)
         sleep(1.2)
     piezoPin.duty_u16(0)
     gPin.off()
     bPin.off()
     rPin.off()
+    lcd.clear()
